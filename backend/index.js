e7a421c9d393beb97c2f6638fb1c8fff0da7f2da
dotcubetitan1 <dotcubetitan1@gmail.com>
2026-03-10 16:05:29 +0530

    d

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,15 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://livechat-frontend-zpcx.onrender.com", 
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
 app.use(express.json());
 
 app.use(authRoutes);
@@ -25,13 +33,6 @@ app.get("/", (req, res) => {
     message: "Server is running!",
     status: "OK" 
   });
-});
-
-
-app.use(express.static(path.join(__dirname, "frontend/dist")));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
 });
 
 const server = http.createServer(app)
